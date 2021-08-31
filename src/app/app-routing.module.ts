30794e97519575d398c9d9d17fa3c3b7421049b0
dipSaha3232 <46640470+dipSaha3232@users.noreply.github.com>
2021-08-31 21:07:58 +0600

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 import { AuthGuardService } from './auth-guard.service';
 import { CorrectAnswerComponent } from './correct-answer/correct-answer.component';
 import { LoginComponent } from './login/login.component';
-import { QuestionComponent } from './question/question.component';
 import { RegisterComponent } from './register/register.component';
 import { StudentGuardService } from './student-guard.service';
 import { StudentPanelComponent } from './student-panel/student-panel.component';
@@ -16,8 +15,7 @@ const routes: Routes = [
   { path : 'admin-panel', component : AdminPanelComponent, canActivate : [AuthGuardService,AdminGuardService]},
   { path : 'student-panel', component : StudentPanelComponent, canActivate : [AuthGuardService,StudentGuardService]},
   { path : 'register', component : RegisterComponent },
-  { path : 'add-question', component : QuestionComponent},
-  { path : 'correct-answers', component : CorrectAnswerComponent}
+  { path : 'correct-answers', component : CorrectAnswerComponent, canActivate : [AuthGuardService,StudentGuardService]}
 
 ];
 
